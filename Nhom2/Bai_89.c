8a7a475452ddaaea4437bb7df6522d270d9a1148
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2025-12-04 02:30:54 +0700

    ok

--- Nhom2/Bai_89.c
+++ Nhom2/Bai_89.c
@@ -1,137 +1,211 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX 1000
+#define MAX 2000
 
-// Ham dao chuoi
-void dao_chuoi(char s[]) {  
-    int i, j;
-    char temp;
-    i = 0;
-    j = strlen(s) - 1;
-    while (i < j) {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
+// Xoa cac so 0 o dau
+void xoaSo0Dau(char *s) {
+    int i = 0;
+    while (s[i] == '0' && s[i+1] != '\0') i++;
+    if (i > 0) memmove(s, s+i, strlen(s)-i+1);
+}
+
+// Xoa cac so 0 o cuoi
+void xoaSo0Cuoi(char *s) {
+    int len = strlen(s);
+    while (len > 1 && s[len-1] == '0') {
+        s[len-1] = '\0';
+        len--;
     }
 }
 
-// Ham xoa dau 0 o dau chuoi ket qua
-void xoa_0_dau(char s[]) {
-    int i = 0, j = 0;
-    while (s[i] == '0' && s[i] != '\0') {
-        i++;
+// Tach so thanh phan nguyen va phan thap phan
+void tachSo(char *num, char *nguyen, char *thapPhan, int *am) {
+    *am = (num[0] == '-');
+
+    char *p = num + (*am ? 1 : 0);
+    char *dot = strchr(p, '.');
+
+    if (dot) {
+        strcpy(nguyen, "");
+        strncat(nguyen, p, dot - p);
+        strcpy(thapPhan, dot + 1);
+    } else {
+        strcpy(nguyen, p);
+        strcpy(thapPhan, "0");
     }
-    while (s[i] != '\0') {
-        s[j++] = s[i++];
-    }
-    s[j] = '\0';
-    if (strlen(s) == 0) {
-        strcpy(s, "0");
-    }
+
+    xoaSo0Dau(nguyen);
+    xoaSo0Cuoi(thapPhan);
 }
 
-// So sanh hai so lon (tra ve 1: a>b, -1: a<b, 0: a=b)
-int so_sanh(char a[], char b[]) {
-    if (strlen(a) > strlen(b)) return 1;
-    if (strlen(a) < strlen(b)) return -1;
-    return strcmp(a, b);
+// So sanh tri tuyet doi 2 so lon
+int soSanhAbs(char *aInt, char *aFrac, char *bInt, char *bFrac) {
+    if (strlen(aInt) != strlen(bInt))
+        return strlen(aInt) > strlen(bInt) ? 1 : -1;
+
+    int cmp = strcmp(aInt, bInt);
+    if (cmp != 0) return cmp > 0 ? 1 : -1;
+
+    int la = strlen(aFrac), lb = strlen(bFrac);
+    int L = la > lb ? la : lb;
+
+    for (int i = 0; i < L; i++) {
+        char ca = (i < la ? aFrac[i] : '0');
+        char cb = (i < lb ? bFrac[i] : '0');
+        if (ca != cb) return ca > cb ? 1 : -1;
+    }
+    return 0;
 }
 
-// Ham cong hai so lon
-void cong_so_lon(char a[], char b[], char kq[]) {
-    char x[MAX], y[MAX];
-    strcpy(x, a);
-    strcpy(y, b);
-    dao_chuoi(x);
-    dao_chuoi(y);
+// Cong tri tuyet doi 2 so lon
+void congAbs(char *aInt, char *aFrac, char *bInt, char *bFrac,
+             char *resInt, char *resFrac)
+{
+    int la = strlen(aFrac);
+    int lb = strlen(bFrac);
+    int L = la > lb ? la : lb;
 
-    int carry = 0, i;
-    int lenx = strlen(x), leny = strlen(y);
-    int maxlen = lenx > leny ? lenx : leny;
+    char A[MAX], B[MAX];
+    strcpy(A, aFrac);
+    strcpy(B, bFrac);
 
-    for (i = 0; i < maxlen; i++) {
-        int so1 = (i < lenx) ? x[i] - '0' : 0;
-        int so2 = (i < leny) ? y[i] - '0' : 0;
-        int tong = so1 + so2 + carry;
-        kq[i] = tong % 10 + '0';
-        carry = tong / 10;
+    while (strlen(A) < L) strcat(A, "0");
+    while (strlen(B) < L) strcat(B, "0");
+
+    resFrac[L] = '\0';
+    int carry = 0;
+
+    for (int i = L-1; i >= 0; i--) {
+        int sum = (A[i] - '0') + (B[i] - '0') + carry;
+        resFrac[i] = sum % 10 + '0';
+        carry = sum / 10;
     }
 
-    if (carry > 0) {
-        kq[i++] = carry + '0';
+    char IA[MAX], IB[MAX];
+    strcpy(IA, aInt);
+    strcpy(IB, bInt);
+
+    int ia = strlen(IA);
+    int ib = strlen(IB);
+    int imax = ia > ib ? ia : ib;
+
+    resInt[imax] = '\0';
+    int k = imax - 1;
+    ia--; ib--;
+
+    while (imax--) {
+        int da = ia >= 0 ? IA[ia--] - '0' : 0;
+        int db = ib >= 0 ? IB[ib--] - '0' : 0;
+        int sum = da + db + carry;
+        resInt[k--] = sum % 10 + '0';
+        carry = sum / 10;
     }
-    kq[i] = '\0';
-    dao_chuoi(kq);
+
+    if (carry) {
+        memmove(resInt + 1, resInt, strlen(resInt) + 1);
+        resInt[0] = '1';
+    }
+
+    xoaSo0Cuoi(resFrac);
+    xoaSo0Dau(resInt);
 }
 
-// Ham tru hai so lon (a - b), gia su a >= b
-void tru_so_lon(char a[], char b[], char kq[]) {
-    char x[MAX], y[MAX];
-    strcpy(x, a);
-    strcpy(y, b);
-    dao_chuoi(x);
-    dao_chuoi(y);
+// Tru tri tuyet doi (so lon tru so be)
+void truAbs(char *aInt, char *aFrac, char *bInt, char *bFrac,
+            char *resInt, char *resFrac)
+{
+    int la = strlen(aFrac), lb = strlen(bFrac);
+    int L = la > lb ? la : lb;
 
-    int lenx = strlen(x), leny = strlen(y);
-    int i, borrow = 0;
+    char A[MAX], B[MAX];
+    strcpy(A, aFrac);
+    strcpy(B, bFrac);
+    while (strlen(A) < L) strcat(A, "0");
+    while (strlen(B) < L) strcat(B, "0");
 
-    for (i = 0; i < lenx; i++) {
-        int so1 = x[i] - '0';
-        int so2 = (i < leny) ? y[i] - '0' : 0;
-        so1 -= borrow;
+    resFrac[L] = '\0';
+    int borrow = 0;
 
-        if (so1 < so2) {
-            so1 += 10;
+    for (int i = L-1; i >= 0; i--) {
+        int da = A[i] - '0' - borrow;
+        int db = B[i] - '0';
+        if (da < db) {
+            da += 10;
             borrow = 1;
-        } else {
-            borrow = 0;
-        }
-
-        kq[i] = (so1 - so2) + '0';
+        } else borrow = 0;
+        resFrac[i] = (da - db) + '0';
     }
 
-    kq[i] = '\0';
-    dao_chuoi(kq);
-    xoa_0_dau(kq);
+    char IA[MAX], IB[MAX];
+    strcpy(IA, aInt);
+    strcpy(IB, bInt);
+
+    int ia = strlen(IA);
+    int ib = strlen(IB);
+    int imax = ia;
+
+    resInt[imax] = '\0';
+    int k = imax - 1;
+    ia--; ib--;
+
+    while (imax--) {
+        int da = IA[ia--] - '0';
+        int db = ib >= 0 ? IB[ib--] - '0' : 0;
+        da -= borrow;
+
+        if (da < db) {
+            da += 10;
+            borrow = 1;
+        } else borrow = 0;
+
+        resInt[k--] = (da - db) + '0';
+    }
+
+    xoaSo0Cuoi(resFrac);
+    xoaSo0Dau(resInt);
 }
 
-// Ham main
 int main() {
-    char a[MAX], b[MAX], kq[MAX];
-    int lua_chon;
+    char A[MAX], B[MAX];
+    char aInt[MAX], aFrac[MAX], bInt[MAX], bFrac[MAX];
+    char resInt[MAX], resFrac[MAX];
+    int signA, signB;
 
     printf("Nhap so thu nhat: ");
-    scanf("%s", a);
+    scanf("%s", A);
     printf("Nhap so thu hai: ");
-    scanf("%s", b);
+    scanf("%s", B);
 
-    printf("Chon phep toan (1: cong, 2: tru): ");
-    scanf("%d", &lua_chon);
+    tachSo(A, aInt, aFrac, &signA);
+    tachSo(B, bInt, bFrac, &signB);
 
-    if (lua_chon == 1) {
-        cong_so_lon(a, b, kq);
-        printf("\nKet qua cong: %s\n", kq);
-    }
-    else if (lua_chon == 2) {
-        int ss = so_sanh(a, b);
-        if (ss == 0) {
-            printf("\nKet qua tru: 0\n");
+    int cmp = soSanhAbs(aInt, aFrac, bInt, bFrac);
+    int signKQ = 0;
+
+    if (signA == signB) {
+        congAbs(aInt, aFrac, bInt, bFrac, resInt, resFrac);
+        signKQ = signA;
+    } else {
+        if (cmp == 0) {
+            printf("Ket qua: 0\n");
+            return 0;
+        } else if (cmp > 0) {
+            truAbs(aInt, aFrac, bInt, bFrac, resInt, resFrac);
+            signKQ = signA;
+        } else {
+            truAbs(bInt, bFrac, aInt, aFrac, resInt, resFrac);
+            signKQ = signB;
         }
-        else if (ss == 1) {  // a > b
-            tru_so_lon(a, b, kq);
-            printf("\nKet qua tru: %s\n", kq);
-        }
-        else {  // a < b -> ket qua am
-            tru_so_lon(b, a, kq);
-            printf("\nKet qua tru: -%s\n", kq);
-        }
-    }
-    else {
-        printf("Lua chon khong hop le!\n");
     }
 
+    if (signKQ) printf("-");
+
+    printf("%s", resInt);
+    if (strlen(resFrac) > 0)
+        printf(".%s", resFrac);
+
+    printf("\n");
     return 0;
 }
